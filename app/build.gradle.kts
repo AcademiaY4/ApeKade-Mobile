1b0b009c3a1fee608e5345a1a028dcd516a18d6b
Vidusha T.Jayaweera <vidushatjayaweera@gmail.com>
2024-10-17 22:46:53 +0530

    rating-review uis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,8 +47,20 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.cardview.v7)
+    implementation (libs.converter.gson)
+    implementation (libs.retrofit)
+    implementation (libs.logging.interceptor)
     implementation(libs.navigation.runtime)
+    implementation(libs.legacy.support.v4)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.github.Inconnu08:android-ratingreviews:1.2.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.code.gson:gson:2.9.1")
+    implementation("com.google.android.material:material:1.5.0")
 }
---
+++ app/src/main/java/com/app/apekade/Activity/SellerInformationActivity.java
@@ -0,0 +1,4 @@
+package com.app.apekade.Activity;
+
+public class SellerInformationActivity {
+}
